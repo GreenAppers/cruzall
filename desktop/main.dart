6742caaf692c79a827a8dd9bc8954e4e07ceb836
GreenAppers <GreenAppers@gmail.com>
2019-07-31 11:08:03 -0700

    Update desktop for internationalization

--- desktop/main.dart
+++ desktop/main.dart
@@ -8,31 +8,47 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:clippy/server.dart' as clippy;
-import 'package:sembast/sembast_io.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:sembast/sembast_io.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'package:cruzawl/preferences.dart';
 
 import 'package:cruzall/app.dart';
 import 'package:cruzall/cruzawl-ui/model.dart';
 
+String assetPath(String asset) => 'assets/$asset';
+
+void setClipboardText(BuildContext context, String text) async =>
+    await clippy.write(text);
+
+void launchUrl(BuildContext context, String url) async {
+  debugPrint('Launching $url');
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    debugPrint('Could not launch $url');
+    setClipboardText(context, url);
+  }
+}
+
 void main() async {
   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   String homePath = Platform.environment['HOME'], dataDirPath = '';
   String appDataPath = Platform.environment['LOCALAPPDATA'];
-  if (homePath != null && homePath.length > 0) dataDirPath = homePath + '/.cruzall';
-  else if (appDataPath != null && appDataPath.length > 0) dataDirPath = appDataPath + '\\Cruzall';
+  if (homePath != null && homePath.length > 0)
+    dataDirPath = homePath + '/.cruzall';
+  else if (appDataPath != null && appDataPath.length > 0)
+    dataDirPath = appDataPath + '\\Cruzall';
   Directory dataDir = Directory(dataDirPath);
   PackageInfo info =
-      PackageInfo('Cruzall', 'com.greenappers.cruzall', '1.0.13', '13');
+      PackageInfo('Cruzall', 'com.greenappers.cruzall', '1.0.14', '14');
   debugPrint('main dataDir=${dataDir.path}');
 
-  SetClipboardText setClipboardText =
-      (BuildContext context, String text) async => await clippy.write(text);
   CruzawlPreferences preferences = CruzawlPreferences(await databaseFactoryIo
       .openDatabase(dataDir.path + Platform.pathSeparator + 'settings.db'));
-  Cruzawl appState = Cruzawl(
-      setClipboardText, databaseFactoryIo, await preferences.load(), dataDir,
+  Cruzawl appState = Cruzawl(assetPath, launchUrl, setClipboardText,
+      databaseFactoryIo, await preferences.load(), dataDir,
       packageInfo: info, isTrustFall: false);
 
   runApp(ScopedModel(
